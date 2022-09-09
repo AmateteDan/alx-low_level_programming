63044a007987253aeb489659e412daa598b9de15
AmateteDan <danjeff09@gmail.com>
2022-09-09 02:56:07 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 	printf("size of a char: %1u byte(s)\n", sizeof(acharacter));
 	printf("size of an int: %1u byte(s)\n", sizeof(ainteger));
 	printf("size of a long int: %1u byte(s)\n", sizeof(along));
-	printf("size of a float: %1u byte(s)\n", sizeof(afloat));
-	printf("size of a longlong int: %1u byte (s)\n", sizeof(alonglong));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("size of a float: %1u byte (s)\n", sizeof(afloat));
 
 	return (0);
 }
