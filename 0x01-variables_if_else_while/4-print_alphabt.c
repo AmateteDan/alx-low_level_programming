a9bfa49df3f93d1199508d2807688999e1c274d7
AmateteDan <danjeff09@gmail.com>
2022-09-11 03:14:23 -0700

    printing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print the alphabet in lowercase letters, except for e and q
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+		if (x != 101 && x != 113)
+	{
+		putchar(x);
+	}
+{
+putchar('\n')
+}
+
+	return (0);
+}
