47dab3da05d55c53555e13d918da203450b0048e
AmateteDan <danjeff09@gmail.com>
2022-09-12 12:15:47 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entrypoint
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+
+	printf("and that piece of art is useful" "-" "Dora Korpar, 2015-10-19,\n");
+
+	return (1);
+}
