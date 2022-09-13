88594df1b966516032af345ee4cbddfd6ef5bcef
AmateteDan <danjeff09@gmail.com>
2022-09-13 00:18:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ *
+ * Description: prints _putchar,followedby a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char sh[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		putchar(sh[x]);
+	}
+	putchar('\n');
+
+	return (0);
+}
