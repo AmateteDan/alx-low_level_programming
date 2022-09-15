847ccfaf4922d120a4cd257e15641ee6bf532149
AmateteDan <danjeff09@gmail.com>
2022-09-15 00:14:13 -0700

    'lowercase'

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - _islower
+ *
+ * @c : variable
+ *
+ * description : checks if it caselower
+ *
+ * Return: int
+ */
+
+int main(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+	}
+}
